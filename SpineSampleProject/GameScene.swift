fca0cf3039f0db64e29020490711fd4a27148deb
omrobbie <putu.roby@yahoo.com>
2019-01-12 15:16:22 +0700

    add bembi from dragonbones

--- SpineSampleProject/GameScene.swift
+++ SpineSampleProject/GameScene.swift
@@ -14,17 +14,39 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
-        if let character = Skeleton(fromJSON: "goblins-ess", atlas: "Goblins", skin: "goblin"){
-            
-            character.name = "character"
+//        if let character = Skeleton(fromJSON: "goblins-ess", atlas: "Goblins", skin: "goblin") {
+//            character.position = CGPoint(x: self.size.width / 2, y: (self.size.height / 2) - 200)
+//
+//            self.addChild(character)
+//
+//            if let walkAnimation = character.animation(named: "walk") {
+//                character.run(SKAction.repeatForever(walkAnimation))
+//            }
+//        }
+
+//        if let character = Skeleton(fromJSON: "goblin", atlas: "Goblin", skin: "default") {
+//            character.position = CGPoint(x: self.size.width / 2, y: (self.size.height / 2) - 200)
+//
+//            self.addChild(character)
+//
+//            if let walkAnimation = character.animation(named: "walk") {
+//                character.run(SKAction.repeatForever(walkAnimation))
+//            }
+//        } else {
+//            print("Gagal load")
+//        }
+
+        if let character = Skeleton(fromJSON: "bembi", atlas: "Bembi", skin: "default") {
             character.position = CGPoint(x: self.size.width / 2, y: (self.size.height / 2) - 200)
-            
+
             self.addChild(character)
-            
-            if let walkAnimation = character.animation(named: "walk") {
-                
+
+            if let walkAnimation = character.animation(named: "geleng") {
                 character.run(SKAction.repeatForever(walkAnimation))
             }
+        } else {
+            print("Gagal load")
         }
+        
     }
 }
